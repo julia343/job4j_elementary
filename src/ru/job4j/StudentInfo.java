20958d38aaced21afa2fa8d06d82accff7f4c4e8
Юлия Буканбаева <julia343@icloud.com>
2020-07-18 19:54:20 +0500

    3.3. Разбор первой программы. [#324056]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Julia Bukanbaeva");
+        System.out.println("05.02.1982");
+    }
+}
