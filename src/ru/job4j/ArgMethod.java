5a787ee09e01fcba29c25d592f184e61f645fb42
Юлия Буканбаева <julia343@icloud.com>
2020-07-22 00:44:04 +0500

    4.1. Аргументы [#324047]

---
+++ src/ru/job4j/ArgMethod.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+public class ArgMethod {
+
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name);
+        System.out.println(age);
+    }
+
+    public static void main(String[] args) {
+        String name = "Petr Arsentev";
+        int age = 33;
+
+        ArgMethod.hello(name, age);
+
+    }
+}
