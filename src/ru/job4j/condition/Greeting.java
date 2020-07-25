cf8e48fba49bd461dc5c2ee15b1d05dece98ea3a
Юлия Буканбаева <julia343@icloud.com>
2020-07-25 21:33:05 +0500

    9.1. String [#324046]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        String separate = " ";
+        String newbie = "But I am a newbie";
+        int year = 2020;
+
+        String id = idea + separate + newbie + separate + year;
+        System.out.println(id);
+    }
+}
