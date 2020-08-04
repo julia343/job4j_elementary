2438446d94b099a4f00c14c1f8710c13a064fc9f
Юлия Буканбаева <julia343@icloud.com>
2020-08-04 19:34:53 +0500

    3. Тесты для определения расстояния между точками. [#324077]
    Signed-off-by: Юлия Буканбаева <julia343@icloud.com>

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -14,14 +14,14 @@ public class PointTest {
 
     @Test
     public void distanceBtoC() {
-        double expected = 7.615773105863909;
+        double expected = 7.62;
         double out = Point.distance(-1, 4, 2, -3);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void distanceCtoD() {
-        double expected = 9.433981132056603;
+        double expected = 9.43;
         double out = Point.distance(-5, 1, 3, 6);
         Assert.assertEquals(expected, out, 0.01);
     }
