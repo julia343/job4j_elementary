492a65affbb2e363e79c7bc4e94f17407f6962c3
Юлия Буканбаева <julia343@icloud.com>
2020-08-06 00:39:34 +0500

    5.4. Протеиновая диета [#324090]
    Signed-off-by: Юлия Буканбаева <julia343@icloud.com>

---
+++ src/ru/job4j/loop/Fitness.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class Fitness {
+
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan < nik) {
+            ivan *= 3;
+            nik *= 2;
+            month = month + 1;
+        }
+        return month;
+    }
+
+    public static void main(String[] args) {
+        int month = Fitness.calc(95, 90);
+        System.out.println(month);
+    }
+}
