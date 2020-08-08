29f47549cfca014242c6235d4a5b99edc9295cc0
Юлия Буканбаева <julia343@icloud.com>
2020-08-08 22:59:11 +0500

    5.6. Простые числа [#324091]
    Signed-off-by: Юлия Буканбаева <julia343@icloud.com>

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(int amount, int salary, double percent) {
+        int year = 0;
+        while (amount >= 0) {
+            amount = (int) (amount - salary + (salary / 100) * percent);
+            year++;
+        }
+        return year;
+    }
+}
