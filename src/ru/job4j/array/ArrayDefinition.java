ea45aae4f5f296d78dd95d8a1fdae3ffbc1c94c4
Юлия Буканбаева <julia343@icloud.com>
2020-08-12 21:42:50 +0500

    6.0.2. Заполнение массива. [#324021]
    Signed-off-by: Юлия Буканбаева <julia343@icloud.com>

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -1,10 +1,16 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
+    public static void main(String[] args) {
 
-    public static void main(String args[] {
-        short[] ages = new short[10];
-        String[] surnames = String[100500];
-        float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        System.out.println(names[0]);
+        names[1] = "Ivan Ivanov";
+        System.out.println(names[1]);
+        names[2] = "Lena Lenina";
+        System.out.println(names[2]);
+        names[3] = "Julia Bukanbaeva";
+        System.out.println(names[3]);
     }
 }
--- test/ru/job4j/loop/MortgageTest.java
+++ test/ru/job4j/loop/MortgageTest.java
@@ -17,4 +17,10 @@ public class MortgageTest {
         int year = Mortgage.year(100, 120, 50);
         assertThat(year, is(2));
     }
+
+    @Test
+    public void when3Year() {
+        int year = Mortgage.year(5000, 1500, 50);
+        assertThat(year, is(7));
+    }
 }
