cdad5d03b8b9d824d048894e6f5fc9f11ea3e84d
Юлия Буканбаева <julia343@icloud.com>
2020-08-20 20:59:45 +0500

    6.4. Слово начинается с ... [#324009]
    Signed-off-by: Юлия Буканбаева <julia343@icloud.com>

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -3,15 +3,12 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        for (int i = 0; i < (word.length / 2); i++) {
-            for (int j = 0; j < pref.length; j++) {
-                if (word[i] == pref[j]) {
-                    result = true;
-                } else {
+        for (int i = 0; i < (word.length); i++) {
+                if (word[i] != pref[i]) {
                     result = false;
+                    break;
                 }
             }
-        }
         return result;
     }
 }
